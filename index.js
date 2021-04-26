3096422bcfcfc76e41a64bd1d6bd2dea95738945
srijan-paul <srijannr1@gmail.com>
2021-04-26 15:04:31 +0530

    now using mongoose

--- index.js
+++ index.js
@@ -1,30 +1,43 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const express = require("express");
+const express = require('express');
 const app = express();
 
-app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(express.static('views'));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json({ limit: "100kb" }));
+app.use(express.json({ limit: '100kb' }));
 
-app.get("/", (req, res) => {
-  res.sendFile("index.html");
-});
-
-app.post("/tweet", (req, res) => {
-  res.send({ status: "success" });
-});
-
-const MongoClient = require("mongodb").MongoClient;
+const MongoClient = require('mongodb').MongoClient;
 const uri = `mongodb+srv://srijanpaul:${process.env.MDB_PASSWORD}@cluster0.5m7qf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-MongoClient.connect(uri, { useUnifiedTopology: true })
-  .then((client) => {
-    console.log("Connected to mongo DB database.");
-    const db = client.db("express-app");
-    const tweetsCollection = db.collection("tweets");
-  })
-  .catch((error) => console.error(error));
+const homePageTweetCount = 10;
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server started on port ${port}`));
+MongoClient.connect(uri, { useUnifiedTopology: true })
+	.then(client => {
+		console.log('Connected to mongo DB database.');
+		const db = client.db('express-app');
+		const tweetsCollection = db.collection('tweets');
+
+		app.get('/', async (_, res) => {
+			const cursor = tweetsCollection.find();
+			cursor.sort({ _id: -1 }).limit(homePageTweetCount);
+
+			try {
+				const tweets = await cursor.toArray();
+				res.render('index.ejs', { tweets });
+			} catch (e) {
+				res.status(200);
+				res.end();
+			}
+		});
+
+		app.post('/tweet', (req, res) => {
+			tweetsCollection.insertOne(req.body).catch(console.error);
+			res.redirect('/');
+		});
+
+		const port = process.env.PORT || 3000;
+		app.listen(port, () => console.log(`Server started on port ${port}`));
+	})
+	.catch(console.error);
